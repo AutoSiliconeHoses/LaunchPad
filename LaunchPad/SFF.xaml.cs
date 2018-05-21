8415ca1ac07003f84d43c5a52fe64ef91e5225a9
Daniel Wise <daniel@autosiliconehoses.com>
2018-05-21 17:21:29 +0100

    Can now launch RunAll() with arguments
    Now RunAll() needs to know what to do with them.

--- LaunchPad/SFF.xaml.cs
+++ LaunchPad/SFF.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,18 +13,53 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace LaunchPad
-{
-    /// <summary>
-    /// Interaction logic for SFF.xaml
-    /// </summary>
-    public partial class SFF : Window
-    {
-        public SFF()
-        {
+namespace LaunchPad {
+    public partial class SFF : Window {
+        public SFF() {
             InitializeComponent();
         }
 
+        private void Run_Click(object sender, RoutedEventArgs e) {
+            string arguments = " ";
+            
+            if (chk_sx.IsChecked.Value == true) {
+                arguments += "sx ";
+            }
+            if (chk_tb.IsChecked.Value == true) {
+                arguments += "tb ";
+            }
+            if (chk_hh.IsChecked.Value == true) {
+                arguments += "hh ";
+            }
+            if (chk_ts.IsChecked.Value == true) {
+                arguments += "ts ";
+            }
+            if (chk_dp.IsChecked.Value == true) {
+                arguments += "dp ";
+            }
+            if (chk_vo.IsChecked.Value == true) {
+                arguments += "vo ";
+            }
+            if (chk_tl.IsChecked.Value == true) {
+                arguments += "tl ";
+            }
+            if (chk_kb.IsChecked.Value == true) {
+                arguments += "kb ";
+            }
+            if (chk_vo.IsChecked.Value == true) {
+                arguments += "dc ";
+            }
+            if (chk_vo.IsChecked.Value == true) {
+                arguments += "dc ";
+            }
+            if (chk_kn.IsChecked.Value == true) {
+                arguments += "kn ";
+            }
 
+            Process.Start("Powershell.exe", @"""& '\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\GUI\runall.ps1'" + arguments);
+
+            //Progress prog = new Progress();
+            //prog.Show();
+        }
     }
 }
