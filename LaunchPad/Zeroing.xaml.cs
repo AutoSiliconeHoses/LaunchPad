3454354fcd7c43ba219f761dfd5bf1b69314039f
Daniel Wise <daniel@autosiliconehoses.com>
2019-04-05 18:33:59 +0100

    Zeroing works for Amazon
    Needs StockMachine access

--- LaunchPad/Zeroing.xaml.cs
+++ LaunchPad/Zeroing.xaml.cs
@@ -9,6 +9,7 @@ namespace LaunchPad
     public partial class Zeroing : Window
     {
 
+        // Define tags/supplier lists
         private List<string> tags = new List<string>();
         private List<string> suppliers = new List<string>();
 
@@ -17,12 +18,14 @@ namespace LaunchPad
             InitializeComponent();
             Loaded += (sender, e) =>
             MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+
+            // Build list of suppliers and their respective tags
             using (var reader = new StreamReader(@"\\DISKSTATION\Feeds\SDK\sku-tag.csv"))
             {
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
-                    var values = line.Split('\t');
+                    var values = line.Split(',');
 
                     tags.Add(values[0]);
                     suppliers.Add(values[1]);
@@ -34,15 +37,19 @@ namespace LaunchPad
         {
             if (e.Key == Key.Return)
             {
-                AddTo();
+                AddList.Text = AddBox.Text + Environment.NewLine + AddList.Text;
                 AddBox.Text = "";
                 e.Handled = true;
             }
         }
 
-        void AddTo()
+        public bool IsDirectoryEmpty(string path)
         {
-            AddList.Text = AddBox.Text + Environment.NewLine + AddList.Text;
+            IEnumerable<string> items = Directory.EnumerateFileSystemEntries(path);
+            using (IEnumerator<string> en = items.GetEnumerator())
+            {
+                return !en.MoveNext();
+            }
         }
 
         private void Btn_clear_click(object sender, RoutedEventArgs e)
@@ -54,19 +61,65 @@ namespace LaunchPad
 
         private void Btn_submit_Click(object sender, RoutedEventArgs e)
         {
+            // Define folder and clear old files
+            DirectoryInfo stockFileFolder = new DirectoryInfo(@"\\DISKSTATION\Feeds\LaunchPad\LaunchPad\Resources\StockFiles");
+            FileInfo[] oldFiles = stockFileFolder.GetFiles();
+            
+            foreach (FileInfo file in oldFiles)
+            {
+                file.Delete();
+            }
+
+            // Get items from AddList
             string listContent = AddList.Text;
             using (StringReader reader = new StringReader(listContent))
             {
                 string stock;
                 while ((stock = reader.ReadLine()) != null)
                 {
+                    // Loop through tags available to find which supplier the sku belongs to
                     foreach (string tag in tags)
                     {
                         if (stock.ToLower().EndsWith(tag))
                         {
                             string supplier = suppliers[tags.IndexOf(tag)];
+                            string path = @"\\DISKSTATION\Feeds\LaunchPad\LaunchPad\Resources\StockFiles\" + supplier + ".txt";
                             System.Diagnostics.Debug.WriteLine(stock + " is a " + supplier + " SKU");
+                            // Create header
+                            if (!File.Exists(@path))
+                            {
+                                File.AppendAllText(@path, "sku\tprice\tminimum-seller-allowed-price\tmaximum-seller-allowed-price\tquantity\tleadtime-to-ship\n");
+                            }
+                            // Add sku to upload file
+                            File.AppendAllText(@path, stock + "\t\t\t\t0\t4\n");
+
+                            // Add sku to alterations file
+                            try
+                            {
+                                File.AppendAllText(@"\\DISKSTATION\Feeds\Stock File Fetcher\StockFeed\GUI\alterations.csv", stock + ",0\n");
+                            } catch (IOException err)
+                            {
+                                MessageBox.Show(err.Message);
+                            }
                         }
+                        
+                    }
+                }
+            }
+            // Check to make sure there were valid skus
+            if (IsDirectoryEmpty(@"\\DISKSTATION\Feeds\LaunchPad\LaunchPad\Resources\StockFiles\")) {
+                System.Diagnostics.Debug.WriteLine("Directory empty");
+            }
+            else
+            {
+                FileInfo[] DispatchFiles = stockFileFolder.GetFiles();
+                foreach (FileInfo file in DispatchFiles)
+                {
+                    if (chk_amazon.IsChecked == true)
+                    {
+                        string originalFileName = file.FullName;
+                        string newFileName = @"\\STOCKMACHINE\AmazonTransport\production\outgoing\ZEROING" + file.Name;
+                        File.Copy(originalFileName, newFileName);
                     }
                 }
             }
